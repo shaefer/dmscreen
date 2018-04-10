c3269fb0ee3416ac725b76db24e9a363e0e843c6
Daniel Shaefer <daniel.shaefer@hudl.com>
2018-04-10 17:28:44 -0500

    Create file to add a few aws scripts for cleaning up/adjusting data in s3

---
+++ src/script/CleanJson.js
@@ -0,0 +1,7 @@
+//https://aws.amazon.com/sdk-for-node-js/
+
+//Lookup all items from bucket s3://cleverorc/pathfinder/v1/monsters
+//get item, JSON.stringify. Resave to a bucket (v2?)
+//Show that this works with Athena (Hadoop only handles single line JSON.)
+
+//Similar Script to rename some of these files?

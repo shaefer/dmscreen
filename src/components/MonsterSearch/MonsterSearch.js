0cbebb2f8c6d991d9d895a630bfdecc7c2ca5ba6
Daniel Shaefer <daniel.shaefer@hudl.com>
2018-06-19 16:54:38 -0500

    Add react-table of searched monsters

--- src/components/MonsterSearch/MonsterSearch.js
+++ src/components/MonsterSearch/MonsterSearch.js
@@ -1,15 +1,62 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux'
 import {monsterS3SelectChangeHandler} from '../../action-creators'
+
 import MonsterSearchForm from './MonsterSearchForm'
+
+import ReactTable from "react-table";
+import "react-table/react-table.css";
 
 const mapMonsters = (monsters) => {
     return monsters.map(x => {
 
+        const strSection = (x.str) ? <span> Str: {x.str}</span> : "";
         const acSection = (x.ac) ? <span> AC: {x.ac}</span> : "";
 
-        return <div>{x.name} CR: {x.cr} Str: {x.str}{acSection}</div>;
+        return <div>{x.name} CR: {x.cr}{strSection}{acSection}</div>;
     });
+}
+
+const mapMonstersToTable = (monsters) => {
+    if (!monsters || (monsters.length == 0)) {
+        monsters = [{name: "No Monsters Found", cr:0, str:1, ac:10}];
+    }
+    let columns = [];
+    columns.push({
+        Header: "Name",
+        accessor: "name"
+    });
+    columns.push({
+        Header: "CR",
+        accessor: "cr"
+    });
+    if (monsters[0].str) columns.push({
+        Header: "Str",
+        accessor: "str"
+    });
+    if (monsters[0].ac) columns.push({
+        Header: "AC",
+        accessor: "ac"
+    });
+
+    return <ReactTable
+    data={monsters}
+    columns={columns}
+    showPagination={false}
+    pageSize={monsters.length}
+    defaultSortDesc={"name"}
+    className="-striped -highlight"/>
+}
+
+const monsterCount = (monsterList) => {
+    if (monsterList) return monsterList.length;
+    return 0;
+}
+
+const addS = (monsterList) => {
+    const count = monsterCount(monsterList);
+    if (count > 1 || count == 0) return "s";
+    return "";
 }
 
 class MonsterSearch extends Component {
@@ -30,9 +77,9 @@ class MonsterSearch extends Component {
         const { config, select, monster, s3Select } = this.props;
         return (
             <div>
-                <span>Hello World</span>
                 <MonsterSearchForm onSubmit={this.submit} />
-                {mapMonsters(s3Select.monsterList)}
+                <h2>Pathfinder Monster Search (Showing {monsterCount(s3Select.monsterList)} monster{addS(s3Select.monsterList)})</h2>
+                {mapMonstersToTable(s3Select.monsterList)}
             </div>
         );
     }
--- src/components/MonsterSearch/MonsterSearchForm.js
+++ src/components/MonsterSearch/MonsterSearchForm.js
@@ -1,50 +1,38 @@
 import React from 'react'
 import { Field, reduxForm } from 'redux-form'
 
+const selectOperatorField = (name) => {
+    return (<Field name={name} component="select">
+    <option>&gt;</option>
+    <option>&gt;=</option>
+    <option>&lt;</option>
+    <option>&lt;=</option>
+    <option>=</option>
+    </Field>);
+}
+
+const searchFieldSection = (prefix, displayLabel) => {
+    return (
+    <div>
+        {selectOperatorField(`${prefix}Operator`)}
+        <label htmlFor={prefix}>{displayLabel}</label>
+        <Field name={prefix} component="input" type="text" />
+    </div>);
+}
+
 let MonsterSearchForm = props => {
   const { handleSubmit } = props
   return (
     <form onSubmit={handleSubmit}>
-      <div>
-        <Field name="crOperator" component="select">
-            <option>&gt;</option>
-            <option>&gt;=</option>
-            <option>&lt;</option>
-            <option>&lt;=</option>
-            <option>=</option>
-        </Field>
-        <label htmlFor="cr">CR</label>
-        <Field name="cr" component="input" type="text" />
-      </div>
-      <div>
-        <Field name="strOperator" component="select">
-            <option>&gt;</option>
-            <option>&gt;=</option>
-            <option>&lt;</option>
-            <option>&lt;=</option>
-            <option>=</option>
-        </Field>
-        <label htmlFor="str">Str</label>
-        <Field name="str" component="input" type="text" />
-      </div>
-      <div>
-        <Field name="acOperator" component="select">
-            <option>&gt;</option>
-            <option>&gt;=</option>
-            <option>&lt;</option>
-            <option>&lt;=</option>
-            <option>=</option>
-        </Field>
-        <label htmlFor="ac">AC</label>
-        <Field name="ac" component="input" type="text" />
-      </div>
+      {searchFieldSection("cr", "CR")}
+      {searchFieldSection("str", "Str")}
+      {searchFieldSection("ac", "AC")}
       <button type="submit">Submit</button>
     </form>
   )
 }
 
 MonsterSearchForm = reduxForm({
-  // a unique name for the form
   form: 'monstersearch',
   initialValues: {
       crOperator: "<=",

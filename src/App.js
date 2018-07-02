3f08aea91d5f819761a4934cd57e2b12b7f39387
Daniel Shaefer <daniel.shaefer@hudl.com>
2018-07-01 19:53:44 -0500

    Work to get basic nav built in. Currently hamburger working on better options

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
 import React, { Component } from 'react';
-import {BrowserRouter, Switch, Route} from 'react-router-dom'
+import {BrowserRouter, Switch, Route, Link} from 'react-router-dom'
 
 import MonsterFinder from './components/MonsterFinder/MonsterFinder'
 import MonsterSearch from './components/MonsterSearch/MonsterSearch'
 import DMScreen from './components/DMScreen/DMScreen'
 
 import './css/CustomFormCss.css'
+import './css/LeftHamburgerNav.css'
 
 class App extends Component {
     render() {
         return (
-            <main>
-            <header className="cleverorc">Clever Orc</header>
             <BrowserRouter>
-                <Switch>
+                <main>
+                    <header className="cleverorc"><div>Clever Orc</div></header>
+                    <input className="nav" type="checkbox" id="navigation" />
+                    <label className="nav" for="navigation"><img src="/images/Hamburger_icon.png"/></label>
+                    <nav className="mainNav">
+                        <ul>
+                            <li><Link to="/">Monster Finder</Link></li>
+                            <li><Link to="/search">Monster Search</Link></li>
+                            <li><Link to="/dmscreen">DM Screen</Link></li>
+                        </ul>
+                    </nav>
+                    <section className="nav">
                     <Route exact path="/"           component={MonsterFinder} />
                     <Route       path="/search"     component={MonsterSearch} />
                     <Route       path="/dmscreen"     component={DMScreen} />
-                </Switch>
+                    </section>
+                </main>
             </BrowserRouter>
-            </main>
         );
     }
 }

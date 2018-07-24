2b3faee93982cf757ad57c506ecd63138ff2ca3a
Daniel Shaefer <daniel.shaefer@hudl.com>
2018-07-24 16:29:35 -0500

    Reverted react-select version to older v1

--- src/components/MonsterFinder/MonsterFinder.js
+++ src/components/MonsterFinder/MonsterFinder.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 
 import './MonsterFinder.css';
-//import 'react-select/dist/react-select.css';
+import 'react-select/dist/react-select.css';
 import '../../css/ReactSelectCustom.css';
 
 import { connect } from 'react-redux'

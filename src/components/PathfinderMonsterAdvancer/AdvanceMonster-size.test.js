c64a1ae52c8bf23b4a143c3f525974bd2a84dfad
Daniel Shaefer <daniel.shaefer@hudl.com>
2019-02-24 11:15:49 -0600

    Added test for descreasing monster size

--- src/components/PathfinderMonsterAdvancer/AdvanceMonster-size.test.js
+++ src/components/PathfinderMonsterAdvancer/AdvanceMonster-size.test.js
@@ -6,8 +6,14 @@ it('advance by size changes main size', () => {
     expect(result.size).toBe('Colossal');
 });
 
-it('advance by size changes ac', () => {
-    console.log("ADVANCE BY SIZE TEST 2", Behir.armor_class.ac_modifiers)
+it('advance by size to a larger size changes ac', () => {
     const result = advanceBySize(Behir, 'Colossal');
     expect(result.ac).toBe('24, touch 3, flat-footed 23 (+1 Dex, +21 natural, -8 size)');
+});
+
+it('advance by size to a smaller size changes ac', () => {
+    //21, touch 9, flat-footed 20 (+1 Dex, +12 natural, -2 size)
+    const result = advanceBySize(Behir, 'Medium');
+    //+4 dex, -5 natural armor, +2 size ac
+    expect(result.ac).toBe('20, touch 13, flat-footed 17 (+3 Dex, +7 natural, +0 size)');
 });

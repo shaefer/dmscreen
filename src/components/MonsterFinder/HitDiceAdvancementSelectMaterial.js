fc19e310d6891a4d3e55eb8d0cdec8b0f41813be
Daniel Shaefer <daniel.shaefer@hudl.com>
2019-02-28 22:55:22 -0600

    removed references to unused imports on the HitDiceSelectWidget

--- src/components/MonsterFinder/HitDiceAdvancementSelectMaterial.js
+++ src/components/MonsterFinder/HitDiceAdvancementSelectMaterial.js
@@ -2,11 +2,8 @@ import React from 'react'
 
 import ReactDOM from 'react-dom';
 import { withStyles } from '@material-ui/core/styles';
-import Input from '@material-ui/core/Input';
 import OutlinedInput from '@material-ui/core/OutlinedInput';
-import FilledInput from '@material-ui/core/FilledInput';
 import InputLabel from '@material-ui/core/InputLabel';
-import FormHelperText from '@material-ui/core/FormHelperText';
 import FormControl from '@material-ui/core/FormControl';
 import Select from '@material-ui/core/Select';
 

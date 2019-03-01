5ba2ecf22e794d053ceaf8b583d1ecca2ccc535c
Daniel Shaefer <daniel.shaefer@hudl.com>
2019-02-28 23:02:51 -0600

    Fixed test for MonsterFinder rendering

--- src/components/MonsterFinder/MonsterFinder.test.js
+++ src/components/MonsterFinder/MonsterFinder.test.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { MonsterFinder } from './MonsterFinder';
-import { PathfinderMonsterAdvancer } from '../PathfinderMonsterAdvancer/PathfinderMonsterAdvancer';
+import PathfinderMonsterAdvancer from '../PathfinderMonsterAdvancer/PathfinderMonsterAdvancer';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   const monster = {statBlock:{name: "Behir", hitDice: 10}}
-  ReactDOM.render(<PathfinderMonsterAdvancer monster={monster}/>, div);
+  ReactDOM.render(<PathfinderMonsterAdvancer monster={monster} advancement={{}}/>, div);
   ReactDOM.unmountComponentAtNode(div);
 });

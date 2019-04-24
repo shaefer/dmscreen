00a615efd232ba93a5b0c669e14383caeefb1bd5
Daniel Shaefer <daniel.shaefer@hudl.com>
2019-04-24 13:59:28 -0500

    Updated cr calculator to handle attacks and damage. Updated the base creatures to have the new breakdowns of attacks

--- src/components/PathfinderMonsterAdvancer/AdvancementTools/ChallengeRatingCalculatorTests/CalculateCr.test.js
+++ src/components/PathfinderMonsterAdvancer/AdvancementTools/ChallengeRatingCalculatorTests/CalculateCr.test.js
@@ -2,5 +2,7 @@ import {calculateCR} from '../ChallengeRatingCalculator'
 import Behir from '../../../../models/Behir_v9'
 
 it('calculateCR works for monster', () => {
-    expect(calculateCR(Behir)).toBe(4.89);
+    expect(Behir.crAsNum).toBe(8)
+    const crs = calculateCR(Behir);
+    expect(crs.total).toBe(5.33);
 });

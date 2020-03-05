952841248ae3b63e00844e3441120124be511262
Daniel Shaefer <shaefer@gmail.com>
2020-03-05 13:55:28 -0600

    Removed unnecessary escape characters and removed console.log

--- src/components/MonsterDisplay.js
+++ src/components/MonsterDisplay.js
@@ -288,7 +288,6 @@ const roundDecimal = (num) => {
 }
 
 const buildCrSection = (m, opts) => {
-    console.log("BUILDING CR SECTION", m, opts)
     const existingAdjustments = (m.crAdjustments) ? m.crAdjustments : [];
     const crAdjustmentsVal = (existingAdjustments.length > 0) ? existingAdjustments.map(x => x.val).reduce((agg, x) => agg + x) : 0;
     const crAdjustmentsText = (existingAdjustments.length > 0) ? <StatBlockLine><B>CR Adjustments</B> {existingAdjustments.map(x => `${x.source} ${withPlus(x.val)}`).join(", ")}</StatBlockLine> : '';
@@ -374,7 +373,7 @@ const MonsterDisplay = ({monster}) => {
     const rangedAttackDisplay = (m.ranged) ? displayFullAttack(m.ranged_attacks) : m.ranged;
     const perceptionSkill = m.skills.find(x => x.name.trim() === 'Perception');
     const perceptionDisplay = (perceptionSkill) ? perceptionSkill.name + ' '  + withPlus(perceptionSkill.value) : '';
-    const senses = (m.senses) ? m.senses.replace(/Perception [\+\-]\d+/, perceptionDisplay) : m.senses;
+    const senses = (m.senses) ? m.senses.replace(/Perception [+-]\d+/, perceptionDisplay) : m.senses;
     const acquiredSpecialAttacksBySource = (acquired) => {
         if (!acquired || !acquired.length === 0) return '';
         return acquired.map(x => <StatBlockLine key={x.source} data={x} required><B>Special Attacks from {x.source}</B> {x.display}</StatBlockLine>)
